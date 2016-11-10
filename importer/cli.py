18d0e76c92d2fc4e23a540f39c0682f7ad4f4854
Dusan Klinec <dusan.klinec@gmail.com>
2016-11-10 13:57:33 +0100

    initial commit 2

---
+++ importer/cli.py
@@ -0,0 +1,302 @@
+from cmd2 import Cmd
+import argparse
+import sys
+import os
+import math
+import types
+import traceback
+import pid
+import time
+import textwrap
+import errors
+from blessed import Terminal
+from core import Core
+from pkg_resources import get_distribution, DistributionNotFound
+from smartcard.System import readers
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class App(Cmd):
+    """EnigmaBridge Importer command line interface"""
+    prompt = '$> '
+
+    PIP_NAME = 'ebimporter.py'
+    PROCEED_YES = 'yes'
+    PROCEED_NO = 'no'
+    PROCEED_QUIT = 'quit'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Init core
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        Cmd.__init__(self, *args, **kwargs)
+        self.core = Core()
+        self.args = None
+        self.last_result = 0
+
+        self.noninteractive = False
+        self.version = self.load_version()
+
+        self.t = Terminal()
+        self.update_intro()
+
+        self.card = None
+
+    def load_version(self):
+        dist = None
+        version = None
+        try:
+            dist = get_distribution(self.PIP_NAME)
+            dist_loc = os.path.normcase(dist.location)
+            here = os.path.normcase(__file__)
+            if not here.startswith(dist_loc):
+                raise DistributionNotFound
+            else:
+                version = dist.version
+        except:
+            version = 'Trunk'
+        return version
+
+    def update_intro(self):
+        self.intro = '-'*self.get_term_width() + \
+                     ('\n    Enigma Bridge Key Importer command line interface (v%s) \n' % self.version) + \
+                     '\n    usage - shows simple command list'
+
+        self.intro += '\n    More info: https://enigmabridge.com/importer \n' + \
+                      '-'*self.get_term_width()
+
+    def do_version(self, line):
+        print('%s-%s' % (self.PIP_NAME, self.version))
+
+    def do_usage(self, line):
+        """TODO: Writes simple usage hints"""
+        print('init   - initializes the PKI key management instance with new identity')
+        print('renew  - renews publicly trusted certificate for secure web access')
+        print('usage  - writes this usage info')
+
+    def do_init(self, line):
+        """
+        test
+        """
+        if not self.check_root() or not self.check_pid():
+            return self.return_code(1)
+
+        # Pick card to use
+        self.select_card()
+
+
+        return self.return_code(1)
+
+    def return_code(self, code=0):
+        self.last_result = code
+        return code
+
+    def cli_sleep(self, iter=5):
+        for lines in range(iter):
+            print('')
+            time.sleep(0.1)
+
+    def select_card(self):
+        rlist = readers()
+        available_readers = []
+        for reader in rlist:
+            try:
+                connection = reader.createConnection()
+                connection.connect()
+                available_readers.append(reader)
+            except:
+                pass
+
+        if len(available_readers) == 0:
+            logger.error("No card readers in the system")
+            return self.return_code(1)
+
+        selected = 0
+        if len(available_readers) > 1:
+            while True:
+                try:
+                    selected = self.select(enumerate(available_readers),
+                                           'Please select the card reader number that should be used: ')
+                    if selected < 0:
+                        raise ValueError('Negative')
+
+                    ok = self.ask_proceed('You choose card reader "%d. %s", is it correct? (y/n): '
+                                          % (selected+1, available_readers[selected]),
+                                          support_non_interactive=True, non_interactive_return=True)
+                    if ok:
+                        break
+
+                except Exception as e:
+                    pass
+
+        self.card = available_readers[selected]
+        return 0
+    
+    def ask_proceed_quit(self, question=None, support_non_interactive=False, non_interactive_return=PROCEED_YES, quit_enabled=True):
+        """Ask if user wants to proceed"""
+        opts = 'Y/n/q' if quit_enabled else 'Y/n'
+        question = question if question is not None else ('Do you really want to proceed? (%s): ' % opts)
+
+        if self.noninteractive and not support_non_interactive:
+            raise errors.Error('Non-interactive mode not supported for this prompt')
+
+        if self.noninteractive and support_non_interactive:
+            if self.args.yes:
+                print(question)
+                if non_interactive_return == self.PROCEED_YES:
+                    print('Y')
+                elif non_interactive_return == self.PROCEED_NO:
+                    print('n')
+                elif non_interactive_return == self.PROCEED_QUIT:
+                    print('q')
+                else:
+                    raise ValueError('Unknown default value')
+
+                return non_interactive_return
+            else:
+                raise errors.Error('Non-interactive mode for a prompt without --yes flag')
+
+        # Classic interactive prompt
+        confirmation = None
+        while confirmation != 'y' and confirmation != 'n' and confirmation != 'q':
+            confirmation = raw_input(question).strip().lower()
+        if confirmation == 'y':
+            return self.PROCEED_YES
+        elif confirmation == 'n':
+            return self.PROCEED_NO
+        else:
+            return self.PROCEED_QUIT
+
+    def ask_proceed(self, question=None, support_non_interactive=False, non_interactive_return=True):
+        """Ask if user wants to proceed"""
+        ret = self.ask_proceed_quit(question=question,
+                                    support_non_interactive=support_non_interactive,
+                                    non_interactive_return=self.PROCEED_YES if non_interactive_return else self.PROCEED_NO,
+                                    quit_enabled=False)
+
+        return ret == self.PROCEED_YES
+
+    def check_root(self):
+        """Checks if the script was started with root - we need that for file ops :/"""
+        uid = os.getuid()
+        euid = os.geteuid()
+        if uid != 0 and euid != 0:
+            print('Error: This action requires root privileges')
+            print('Please, start the client with: sudo -E -H ebaws')
+            return False
+        return True
+
+    def check_pid(self, retry=True):
+        """Checks if the tool is running"""
+        first_retry = True
+        attempt_ctr = 0
+        while first_retry or retry:
+            try:
+                first_retry = False
+                attempt_ctr += 1
+
+                self.core.pidlock_create()
+                if attempt_ctr > 1:
+                    print('\nPID lock acquired')
+                return True
+
+            except pid.PidFileAlreadyRunningError as e:
+                return True
+
+            except pid.PidFileError as e:
+                pidnum = self.core.pidlock_get_pid()
+                print('\nError: CLI already running in exclusive mode by PID: %d' % pidnum)
+
+                if self.args.pidlock >= 0 and attempt_ctr > self.args.pidlock:
+                    return False
+
+                print('Next check will be performed in few seconds. Waiting...')
+                time.sleep(3)
+        pass
+
+    def get_term_width(self):
+        try:
+            width = self.t.width
+            if width is None or width <= 0:
+                return 80
+
+            return width
+        except:
+            pass
+        return 80
+
+    def wrap_term(self, text="", single_string=False, max_width=None):
+        width = self.get_term_width()
+        if max_width is not None and width > max_width:
+            width = max_width
+
+        res = textwrap.wrap(text, width)
+        return res if not single_string else '\n'.join(res)
+
+    def app_main(self):
+        # Backup original arguments for later parsing
+        args_src = sys.argv
+
+        # Parse our argument list
+        parser = argparse.ArgumentParser(description='EnigmaBridge AWS client')
+        parser.add_argument('-n', '--non-interactive', dest='noninteractive', action='store_const', const=True,
+                            help='non-interactive mode of operation, command line only')
+        parser.add_argument('-r', '--attempts', dest='attempts', type=int, default=3,
+                            help='number of attempts in non-interactive mode')
+        parser.add_argument('-l','--pid-lock', dest='pidlock', type=int, default=-1,
+                            help='number of attempts for pidlock acquire')
+        parser.add_argument('--debug', dest='debug', action='store_const', const=True,
+                            help='enables debug mode')
+        parser.add_argument('--verbose', dest='verbose', action='store_const', const=True,
+                            help='enables verbose mode')
+        parser.add_argument('--force', dest='force', action='store_const', const=True, default=False,
+                            help='forces some action (e.g., certificate renewal)')
+        parser.add_argument('--email', dest='email', default=None,
+                            help='email address to use instead of prompting for one')
+
+        parser.add_argument('--yes', dest='yes', action='store_const', const=True,
+                            help='answers yes to the questions in the non-interactive mode, mainly for init')
+
+        parser.add_argument('--allow-update', action='store_const', const=True,
+                            help='Inherited option from auto-update wrapper, no action here')
+        parser.add_argument('--no-self-upgrade', action='store_const', const=True,
+                            help='Inherited option from auto-update wrapper, no action here')
+        parser.add_argument('--os-packages-only', action='store_const', const=True,
+                            help='Inherited option from auto-update wrapper, no action here')
+
+        parser.add_argument('commands', nargs=argparse.ZERO_OR_MORE, default=[],
+                            help='commands to process')
+
+        self.args = parser.parse_args(args=args_src[1:])
+        self.noninteractive = self.args.noninteractive
+
+        # Fixing cmd2 arg parsing, call cmdLoop
+        sys.argv = [args_src[0]]
+        for cmd in self.args.commands:
+            sys.argv.append(cmd)
+
+        # Terminate after execution is over on the non-interactive mode
+        if self.noninteractive:
+            sys.argv.append('quit')
+
+        self.cmdloop()
+        sys.argv = args_src
+
+        # Noninteractive - return the last result from the operation (for scripts)
+        if self.noninteractive:
+            sys.exit(self.last_result)
+
+
+def main():
+    app = App()
+    app.app_main()
+
+
+if __name__ == '__main__':
+    main()
